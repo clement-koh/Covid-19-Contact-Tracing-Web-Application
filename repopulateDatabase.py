3d60dac52305ed8673fca698d2e4f4797ef68d3e
LemonzTea <54799314+LemonzTea@users.noreply.github.com>
2021-04-24 22:01:14 +0800

    Update location to generate database

--- repopulateDatabase.py
+++ repopulateDatabase.py
@@ -16,7 +16,7 @@ app = Flask(__name__)
 
 # Database Settings
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app/db.sqlite3'
 
 db = SQLAlchemy(app)
 
