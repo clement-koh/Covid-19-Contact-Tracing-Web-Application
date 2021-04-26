051d5cb7e51c9f385b5f797b87aaec4c6e09da04
LemonzTea <54799314+LemonzTea@users.noreply.github.com>
2021-04-27 01:35:36 +0800

    Replaced Controller file

---
+++ app/controllers/HealthStaffUser_ViewPatientDetailsController.py
@@ -0,0 +1,43 @@
+from ..entity.User import User
+
+class HealthStaffUser_ViewPatientDetailsController:
+	def __init__(self):
+		pass
+
+	def getPatientList(self):
+		# Creates a user object
+		user = User()
+		return user.getAllNRIC()
+
+	def verifyPatient(self, NRIC):
+		# Creates a user object
+		user = User(NRIC)
+
+		if user.getNRIC() is None:
+			return False
+		return True
+
+	def getPatientDetails(self, NRIC):
+		""" Returns a list containing the following information.
+
+		[0] - NRIC, 
+		[1] - First Name, 
+		[2] - Middle Name, 
+		[3] - Last Name, 
+		[4] - Gender, 
+		[5] - Mobile Number, 
+		"""
+
+		# Creates a user object
+		user = User(NRIC)
+		
+		# Returns all details in an array
+		userInfo = []
+		userInfo.append(NRIC)
+		userInfo.append(user.getFirstName())
+		userInfo.append(user.getMiddleName())
+		userInfo.append(user.getLastName())
+		userInfo.append(user.getGender())
+		userInfo.append(user.getMobile())
+		
+		return userInfo
