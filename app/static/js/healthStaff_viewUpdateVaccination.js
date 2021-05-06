211c3b00af898c76db8178df94ab386a04877ad5
LemonzTea <54799314+LemonzTea@users.noreply.github.com>
2021-05-06 23:42:17 +0800

    Set disabled for options that are guarenteed, force checkbox checked for vaccinated status

--- app/static/js/healthStaff_viewUpdateVaccination.js
+++ app/static/js/healthStaff_viewUpdateVaccination.js
@@ -1,6 +1,15 @@
+var statusDropdown;
+var checkbox_firstDose;
+var checkbox_secondDose;
+
 $(document).ready(function() {
 	// Add a change listener
-	$("#status_dropdown").change(updateOptions);
+	statusDropdown = $("#status_dropdown")
+	checkbox_firstDose = $("#first_dose");
+	checkbox_secondDose = $("#second_dose");
+	statusDropdown.change(updateOptions);
+	checkbox_firstDose.change(forceCheckBox);
+	checkbox_secondDose.change(forceCheckBox);
 	updateOptions();
 })
 
@@ -12,26 +21,31 @@ options = ['not_eligible',
 
 // Update the option based on the dropdown selected
 function updateOptions() {
-	var selectedOption = $("#status_dropdown").children("option:selected").val();
-	var checkbox_firstDose = $("#first_dose");
-	var checkbox_secondDose = $("#second_dose");
+	var selectedOption = statusDropdown.children("option:selected").val();
 
 	// Uncheck all checkboxes
 	checkbox_firstDose.attr("disabled", true);
 	checkbox_secondDose.attr("disabled", true);
-	checkbox_firstDose.prop("checked", false );
-	checkbox_secondDose.prop("checked", false );
+	checkbox_firstDose.prop("checked", false);
+	checkbox_secondDose.prop("checked", false);
 
 	if (selectedOption == options[3]) {
 		// Set first_dose as scheduled
-		checkbox_firstDose.prop("checked", true );
 		checkbox_firstDose.attr("disabled", false);
 
 	} else if (selectedOption == options[4]) {
-		checkbox_firstDose.prop("checked", true );
-		checkbox_secondDose.prop("checked", true );
+		checkbox_firstDose.prop("checked", true);
+		checkbox_secondDose.prop("checked", true);
 		checkbox_firstDose.attr("disabled", false);
 		checkbox_secondDose.attr("disabled", false);
 	}
 }
 
+function forceCheckBox(check) {
+	var selectedOption = statusDropdown.children("option:selected").val();
+
+	if (selectedOption == options[4]) {
+		checkbox_firstDose.prop("checked", true);
+		checkbox_secondDose.prop("checked", true);
+	}
+}
