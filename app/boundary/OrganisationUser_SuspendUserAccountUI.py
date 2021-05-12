a71b7e246a549d649ab2c8abc52a3cbb6fe1e05f
LemonzTea <54799314+LemonzTea@users.noreply.github.com>
2021-05-12 21:58:17 +0800

    Prevent unauthorised access based on user type, added session to store the user being edited, Edited Success message

--- app/boundary/OrganisationUser_SuspendUserAccountUI.py
+++ app/boundary/OrganisationUser_SuspendUserAccountUI.py
@@ -8,21 +8,25 @@ class OrganisationUser_SuspendUserAccountUI:
 	def __init__(self):
 		pass
 
-
-	def onSubmit(self, NRIC, AccountStatus):
+	def onSubmit(self, NRIC):
 		"""
 		Updates Account Status to suspend or not.
 		Returns True if the Account status is updated successfully.
 		"""
+		# Ensure the user is authorised to perform this functionality
+		userType = session['userType']
+		if userType != "Organisation":
+			flash("Unauthorised access to this content", "error")
+			return redirect("/")
 
 		# Set session to record the user viewed
-		session['viewVaccinationInformation'] = NRIC
+		session['viewingNRIC'] = NRIC
 		
 		# Create controller to update SuspendAccount Status
 		controller = OrganisationUser_SuspendUserAccountController()
 
 		# Calls the controller to update SuspendAccount status
-		result = controller.updateAccountStatus(NRIC, AccountStatus)
+		result = controller.updateAccountStatus(NRIC)
 		
 		# Returns True if successfully updated / False if failed to update
 		return result
@@ -33,8 +37,10 @@ class OrganisationUser_SuspendUserAccountUI:
 		with an error message
 		"""
 
+		# Error message to be displayed
 		message = "Failed to Change Account Status"
 
+		# Flask error message
 		flash(message, 'error')
 
 		#redirect to ViewUserAccount route
@@ -46,10 +52,10 @@ class OrganisationUser_SuspendUserAccountUI:
 		Display message to inform the User account status had successful change
 		"""
 
-		#store successful message in session 
-		message = "Account Status Change Successful"
+		# store successful message
+		message = "Account suspension status has been updated successfully"
 
-		#Flash Successful message
+		# Flash Successful message
 		flash(message, 'message')
 
 		#redirect to ViewUserAccount route
