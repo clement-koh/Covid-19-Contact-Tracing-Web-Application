f11e9f0caadfb26fb103f290b1e4503a98867d92
LemonzTea <54799314+LemonzTea@users.noreply.github.com>
2021-05-12 21:59:13 +0800

    Cleaned up code to toggle account status

--- app/controllers/OrganisationUser_SuspendUserAccountController.py
+++ app/controllers/OrganisationUser_SuspendUserAccountController.py
@@ -6,20 +6,15 @@ class OrganisationUser_SuspendUserAccountController:
 		pass
 
 
-	def updateAccountStatus(self, NRIC, AccountStatus):
+	def updateAccountStatus(self, NRIC):
 		"""
 		Updates the Account Status of the user.
 		Returns True if successfully updated.
 		Returns False if unsuccessful
 		"""		
-
+		
 		# Create a AccountStatus object containing details of the NRIC owner
-		AccountActiveStatus = User(NRIC)
+		user = User(NRIC)
 
-		# Returns True if account is active return false if not.
-		if AccountActiveStatus.getAccountActive():
-			AccountStatus = True
-		else:
-			AccountStatus = False
-
-		return AccountActiveStatus.updateAccountStatus(NRIC, AccountStatus)
+		# Toggles the account active status for the user
+		return user.updateAccountActive()
