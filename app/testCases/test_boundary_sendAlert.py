8a83890e35ea3b6bf52155f9b1480de9cdf8bfef
LemonzTea <54799314+LemonzTea@users.noreply.github.com>
2021-05-20 20:36:05 +0800

    Updated format and added comments

--- app/testCases/test_boundary_sendAlert.py
+++ app/testCases/test_boundary_sendAlert.py
@@ -19,58 +19,92 @@ class testCases_HealthStaffUser_SendAlertPublicUI(unittest.TestCase):
 		self.maxDiff = None
 
 	def test_displayPage_allowsAccessForHealthStaffUser(self):
+		# Setting Session Context
 		with self.app.test_request_context() as c:
 			session['userType'] = 'Health Staff'
+
 			result = self.boundary.displayPage()
-			self.assertEqual(result, render_template('healthStaff_new_public_alert.html', userType="Health Staff"), "Failed to display the expected webpage")
+			expectedResult = render_template('healthStaff_new_public_alert.html', userType="Health Staff")
+			errorMsg = "Failed to display the expected webpage"
+			self.assertEqual(result, expectedResult, errorMsg)
 	
 	def test_displayPage_preventsAccessForPublicUser(self):
+		# Setting Session Context
 		with self.app.test_request_context() as c:
 			session['userType'] = 'Public'
+
 			result = self.boundary.displayPage()
-			self.assertNotEqual(result, render_template('healthStaff_new_public_alert.html', userType="Public"), "Public User has unauthorized access to webpage")
+			expectedResult = render_template('healthStaff_new_public_alert.html', userType="Public")
+			errorMsg = "Public User has unauthorized access to webpage"
+			self.assertNotEqual(result, expectedResult, errorMsg)
 
 	def test_displayPage_preventsAccessForBusinessUser(self):
+		# Setting Session Context
 		with self.app.test_request_context() as c:
 			session['userType'] = 'Business'
+
 			result = self.boundary.displayPage()
-			self.assertNotEqual(result, render_template('healthStaff_new_public_alert.html', userType="Business"), "Business User has unauthorized access to webpage")
+			expectedResult = render_template('healthStaff_new_public_alert.html', userType="Business")
+			errorMsg = "Business User has unauthorized access to webpage"
+			self.assertNotEqual(result, expectedResult, errorMsg)
 
 	def test_displayPage_preventsAccessForOrganisationUser(self):
+		# Setting Session Context
 		with self.app.test_request_context() as c:
 			session['userType'] = 'Organisation'
+
 			result = self.boundary.displayPage()
-			self.assertNotEqual(result, render_template('healthStaff_new_public_alert.html', userType="Organisation"), "Organisation User has unauthorized access to webpage")
+			expectedResult = render_template('healthStaff_new_public_alert.html', userType="Organisation")
+			errorMsg = "Organisation User has unauthorized access to webpage"
+			self.assertNotEqual(result, expectedResult, errorMsg)
 
 	def test_onSubmit_acceptCorrectValues(self):
+		# Setting Session Context
 		with self.app.test_request_context() as c:
 			session['user'] = 'S1999'
+
 			result = self.boundary.onSubmit("S0999", "Message from BOUNDARY test case with correct values")
-			self.assertEqual(result, "Alert successfully sent to 'S0999'", "onSubmit with correct values did not return Success")
+			expectedResult = "Alert successfully sent to 'S0999'"
+			errorMsg = "onSubmit with correct values did not return Success"
+			self.assertEqual(result, expectedResult, errorMsg)
 	
 	def test_onSubmit_rejectIncorrectNRIC(self):
 		result = self.boundary.onSubmit("S9999", "Message from test case with incorrect NRIC")
-		self.assertEqual(result, "Recipient('S9999') is not a valid user", "onSubmit with incorrect NRIC did not return the correct error")
+		expectedResult = "Recipient('S9999') is not a valid user"
+		errorMsg = "onSubmit with incorrect NRIC did not return the correct error"
+		self.assertEqual(result, expectedResult, errorMsg)
 
 	def test_onSubmit_rejectEmptyNRIC(self):
 		result = self.boundary.onSubmit("", "Message with empty NRIC")
-		self.assertEqual(result, "Fields cannot be empty", "onSubmit with blank NRIC did not return the correct error")
+		expectedResult = "Fields cannot be empty"
+		errorMsg = "onSubmit with blank NRIC did not return the correct error"
+		self.assertEqual(result, expectedResult, errorMsg)
 
 	def test_onSubmit_rejectEmptyMessage(self):
 		result = self.boundary.onSubmit("S0999", "")
-		self.assertEqual(result, "Fields cannot be empty", "onSubmit with blank message did not return the correct error")
+		expectedResult = "Fields cannot be empty"
+		errorMsg = "onSubmit with blank message did not return the correct error"
+		self.assertEqual(result, expectedResult, errorMsg)
 
 	def test_displayError_displayErrorPage(self):
+		# Setting Session Context
 		with self.app.test_request_context() as c:
 			session['userType'] = 'Health Staff'
+
 			result = self.boundary.displayError("error")
-			self.assertEqual(result, render_template('healthStaff_new_public_alert.html', userType="Health Staff"), "Failed to display the expected webpage")
+			expectedResult = render_template('healthStaff_new_public_alert.html', userType="Health Staff")
+			errorMsg = "Failed to display the expected webpage"
+			self.assertEqual(result, expectedResult, errorMsg)
 
 	def test_displaySuccess_displaySuccessPage(self):
+		# Setting Session Context
 		with self.app.test_request_context() as c:
 			session['userType'] = 'Health Staff'
+
 			result = self.boundary.displaySuccess()
-			self.assertEqual(result, render_template('healthStaff_new_public_alert.html', userType="Health Staff"), "Failed to display the expected webpage")
+			expectedResult = render_template('healthStaff_new_public_alert.html', userType="Health Staff")
+			errorMsg = "Failed to display the expected webpage"
+			self.assertEqual(result, expectedResult, errorMsg)
 
 if __name__ == '__main__':
 	unittest.main(verbosity=2)
--- app/testCases/test_controller_sendAlert.py
+++ app/testCases/test_controller_sendAlert.py
@@ -7,65 +7,94 @@ class testCases_HealthStaffUser_SendAlertPublicController(unittest.TestCase):
 		# Create boundary object
 		self.controller = HealthStaffUser_SendAlertPublicController()
 
+	# ----------------------------------------------------
+	#					Test verifyNRIC method
+	# ----------------------------------------------------
+
+	# Test with correct value
 	def test_verifyNRIC_acceptsPublicNRIC(self):
+		errorMsg = "Public user's NRIC provided, expected True but returned False"
 		result = self.controller.verifyNRIC("S0999")
-		self.assertTrue(result, "Public user's NRIC provided, expected True but returned False")
+		self.assertTrue(result, errorMsg)
 	
+	# Test with incorrect value
 	def test_verifyNRIC_rejectsHealthStaffUserNRIC(self):
+		errorMsg = "Health Staff user's NRIC provided, expected False but returned True"
 		result = self.controller.verifyNRIC("S1999")
-		self.assertFalse(result, "Health Staff user's NRIC provided, expected False but returned True")
+		self.assertFalse(result, errorMsg)
 
 	def test_verifyNRIC_rejectsBusinessUserNRIC(self):
+		errorMsg = "Business user's NRIC provided, expected False but returned True"
 		result = self.controller.verifyNRIC("S2999")
-		self.assertFalse(result, "Business user's NRIC provided, expected False but returned True")
+		self.assertFalse(result, errorMsg)
 
 	def test_verifyNRIC_rejectsOrganisationUserNRIC(self):
+		errorMsg = "Organisation user's NRIC provided, expected False but returned True"
 		result = self.controller.verifyNRIC("S3999")
-		self.assertFalse(result, "Organisation user's NRIC provided, expected False but returned True")
+		self.assertFalse(result, errorMsg)
 	
 	def test_verifyNRIC_rejectsInvalidNRIC(self):
+		errorMsg = "Invalid NRIC provided, expected False but returned True"
 		result = self.controller.verifyNRIC("invalidNRIC")
-		self.assertFalse(result, "Invalid NRIC provided, expected False but returned True")
+		self.assertFalse(result, errorMsg)
 
 	def test_verifyNRIC_rejectsBlankNRIC(self):
+		errorMsg = "Blank NRIC provided, expected False but returned True"
 		result = self.controller.verifyNRIC("")
-		self.assertFalse(result, "Blank NRIC provided, expected False but returned True")
+		self.assertFalse(result, errorMsg)
 
+	# ----------------------------------------------------
+	#					Test sendAlert method
+	# ----------------------------------------------------
+
+	# Test with correct value
 	def test_sendAlert_acceptCorrectValues(self):
+		errorMsg = "Correct values provided, expected True but returned False"
 		result = self.controller.sendAlert("S0999", "Message from CONTROLLER test case with correct values", "S1999")
-		self.assertTrue(result, "Correct values provided, expected True but returned False")
+		self.assertTrue(result, errorMsg)
 
+	# Test with incorrect RECIPIENT values
 	def test_sendAlert_rejectBlankRecipient(self):
+		errorMsg = "Invalid NRIC provided, expected False but returned True"
 		result = self.controller.sendAlert("", "Message from CONTROLLER test case without recipient NRIC", "S1999")
-		self.assertFalse(result, "Invalid NRIC provided, expected False but returned True")
+		self.assertFalse(result, errorMsg)
 
 	def test_sendAlert_rejectInvalidRecipient(self):
+		errorMsg = "Invalid NRIC provided, expected False but returned True"
 		result = self.controller.sendAlert("invalidNRIC", "Message from CONTROLLER test case with incorrect NRIC", "S1999")
-		self.assertFalse(result, "Invalid NRIC provided, expected False but returned True")
+		self.assertFalse(result, errorMsg)
 
+	# Test with incorrect MESSAGE values
 	def test_sendAlert_rejectBlankMessage(self):
+		errorMsg = "No alert message provided, expected False but returned True"
 		result = self.controller.sendAlert("S0999", "", "S1999")
-		self.assertFalse(result, "No alert message provided, expected False but returned True")
+		self.assertFalse(result, errorMsg)
 
+	# Test with incorrect SENDER values
 	def test_sendAlert_rejectBlankSender(self):
+		errorMsg = "No sender NRIC provided, expected False but returned True"
 		result = self.controller.sendAlert("S0999", "Message from CONTROLLER test case without sender NRIC", "")
-		self.assertFalse(result, "No sender NRIC provided, expected False but returned True")
+		self.assertFalse(result, errorMsg)
 
 	def test_sendAlert_rejectInvalidSender(self):
+		errorMsg = "Invalid NRIC provided, expected False but returned True"
 		result = self.controller.sendAlert("S0999", "Message from CONTROLLER test case with invalid NRIC", "InvalidNRIC")
-		self.assertFalse(result, "Invalid NRIC provided, expected False but returned True")
+		self.assertFalse(result, errorMsg)
 
 	def test_sendAlert_rejectPublicUserAsSender(self):
+		errorMsg = "Public User's NRIC provided as sender, expected False but returned True"
 		result = self.controller.sendAlert("S0999", "Message from CONTROLLER test case by sent by public user", "S0999")
-		self.assertFalse(result, "Public User's NRIC provided as sender, expected False but returned True")
+		self.assertFalse(result, errorMsg)
 
 	def test_sendAlert_rejectBusinessUserAsSender(self):
+		errorMsg = "Business User's NRIC provided as sender, expected False but returned True"
 		result = self.controller.sendAlert("S0999", "Message from CONTROLLER test case by sent by business user", "S2999")
-		self.assertFalse(result, "Business User's NRIC provided as sender, expected False but returned True")
+		self.assertFalse(result, errorMsg)
 	
 	def test_sendAlert_rejectOrganisationUserAsSender(self):
+		errorMsg = "Organisation User's NRIC provided as sender, expected False but returned True"
 		result = self.controller.sendAlert("S0999", "Message from CONTROLLER test case by sent by organisation user", "S3999")
-		self.assertFalse(result, "Organisation User's NRIC provided as sender, expected False but returned True")
+		self.assertFalse(result, errorMsg)
 
 if __name__ == '__main__':
 	unittest.main(verbosity=2)
