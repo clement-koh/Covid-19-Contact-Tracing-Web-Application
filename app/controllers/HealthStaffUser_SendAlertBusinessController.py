e9ab9565052badcbba55c99a8a9ca45acac77adb
unknown <jingkaihere@hotmail.com>
2021-05-26 01:19:13 +0800

    update sendAlertBusiness comment

--- app/controllers/HealthStaffUser_SendAlertBusinessController.py
+++ app/controllers/HealthStaffUser_SendAlertBusinessController.py
@@ -30,7 +30,7 @@ class HealthStaffUser_SendAlertBusinessController:
 			if businessName == name:
 				validationCode = 0
 		
-		# Check if business name exist
+		# Check if business name does not exist
 		if validationCode == -1:
 			return validationCode
 
