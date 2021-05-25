b287911dabf18156296551119097373d227953a2
dannbumm <danielchuazh93@gmail.com>
2021-05-26 00:21:42 +0800

    Code Changes for View Location History

--- app/boundary/PublicUser_ViewLocationHistoryUI.py
+++ app/boundary/PublicUser_ViewLocationHistoryUI.py
@@ -20,37 +20,13 @@ class PublicUser_LocationHistoryUI:
 		# Create Public User's Location History controller for the current user
 		controller = PublicUser_LocationHistoryController()
 
+
+
+
 		# Get the details for the current User
-		results = controller.getLocationHistory(session['user'])
+		locationHistory = controller.getLocationHistory(session['user'])
 
-		# Create an empty array
-		locationHistory = []
-
-		# Check if result is empty
-		if results is not None:
-			# Populate the dictionary after formating the results to be display
-			for result in results:
-				
-				checkInTime = datetime.strptime(result[3], '%Y-%m-%d %H:%M:%S')
-				checkoutTime = datetime.strptime(result[4], '%Y-%m-%d %H:%M:%S')
-
-				# Create an empty dictionary
-				record = {}
-
-				# Records the location name
-				record['locationName'] = controller.getLocationName(result[2])
-				
-				# Gets the date
-				record['date'] = checkInTime.strftime('%d %b %Y') 
-				
-				# Format the time in 24 hours timing (eg. 13:45)
-				record['time_in'] = '{:02d}:{:02d}'.format(checkInTime.hour,
-														   checkInTime.minute)
-				record['time_out'] = '{:02d}:{:02d}'.format(checkoutTime.hour,
-														    checkoutTime.minute)
-
-				# Add the dictionary into the array
-				locationHistory.append(record)
+		
 
 		# Displays the webpage with formatted data
 		return render_template('public_viewLocationHistory.html', userType=session['userType'],
--- app/controllers/PublicUser_LocationHistoryController.py
+++ app/controllers/PublicUser_LocationHistoryController.py
@@ -1,5 +1,6 @@
 from ..entity.LocationHistory import LocationHistory
 from ..entity.Location import Location
+from datetime import datetime
 
 class PublicUser_LocationHistoryController:
 	# Constructor
@@ -28,7 +29,45 @@ class PublicUser_LocationHistoryController:
 		"""
 		
 		# Returns the result of the query
-		return self.__locationHistory.getPastLocationHistory(NRIC, self.HISTORY_NUMBER_OF_DAYS)
+		results = self.__locationHistory.getPastLocationHistory(NRIC, self.HISTORY_NUMBER_OF_DAYS)
+
+
+		# Create an empty array
+		locationHistory = []
+
+		# Check if result is empty
+		if results is not None:
+			# Populate the dictionary after formating the results to be display
+			for result in results:
+				
+				checkInTime = datetime.strptime(result[3], '%Y-%m-%d %H:%M:%S')
+				checkoutTime = datetime.strptime(result[4], '%Y-%m-%d %H:%M:%S')
+
+				# Create an empty dictionary
+				record = {}
+
+				# Records the location name
+				record['locationName'] = self.getLocationName(result[2])
+				
+				# Gets the date
+				record['date'] = checkInTime.strftime('%d %b %Y') 
+				
+				# Format the time in 24 hours timing (eg. 13:45)
+				record['time_in'] = '{:02d}:{:02d}'.format(checkInTime.hour,
+														   checkInTime.minute)
+				record['time_out'] = '{:02d}:{:02d}'.format(checkoutTime.hour,
+														    checkoutTime.minute)
+
+				# Add the dictionary into the array
+				locationHistory.append(record)
+
+		return locationHistory
+
+
+
+
+
+
 
 	def getLocationName(self, id):
 		"""
